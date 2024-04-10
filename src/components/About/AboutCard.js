a10afe5212f68aa8c29269095a92c5950ce5e7e8
saumya9889 <msaumya114@gmail.com>
2024-04-10 13:12:53 +0530

    update

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,22 +8,23 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Saumya Mishra</span>
-            from <span className="purple"> Gorakhpur, India.</span>
-            <br /> I am a second year student pursuing  MCA
-             and Computing at GD Goenka University.
-            <br />
-            I am a frontend web developer. I can provide clean code and pixel perfect design. I also make website more & more interactive with web animations.
-            <br />
-            <br />
-            Apart from coding, some other activities that I love to do!
+            Hi Everyone, I am <span className="purple">Saumya Mishra.</span>
+            {/* from <span className="purple"> Gorakhpur, India.</span> */}
+            <br/>
+            Aspiring Front-End Developer | Passionate About Crafting User-Friendly.
+            <br/>
+             I’m Saumya Mishra, a budding front-end devel-oper passionate about 
+             <br/>creating visually appealing and responsive web interfaces. 
+             <br/> I’m on a journey to turn my passionfor coding into beautiful, user-centric websites.
+             <br/>
+             When not working on projects, you can find me 
           </p>
           <ul>
             <li className="about-activity">
               <ImPointRight /> Playing Games
             </li>
             <li className="about-activity">
-              <ImPointRight /> Writing Tech Blogs
+              <ImPointRight /> Creating Content
             </li>
             <li className="about-activity">
               <ImPointRight /> Travelling
